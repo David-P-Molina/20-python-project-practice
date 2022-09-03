fe0a27636d73325cb3de4a5bdbe5152bb82c2538
David Moina <davemolina777@gmail.com>
2022-09-03 16:05:09 -0500

    Add notes to project

--- word_replacement_program.py
+++ word_replacement_program.py
@@ -1,8 +1,8 @@
-def replace_word():
-    
-    str = "Hi guys, I am David, and hi hi hi hi"
-    word_to_replace = input("Enter the word you wish to replace")
+def replace_word(): #how to define a method
+    #indentation defines open and closing of function/methods
+    str = "Hi guys, I am David, and hi hi hi hi" #Create variables regardless of datatypes
+    word_to_replace = input("Enter the word you wish to replace")#input is a function that takes user input
     word_replacer = input("Enter new word taking place")
-    print(str.replace(word_to_replace, word_replacer))
+    print(str.replace(word_to_replace, word_replacer))#use print() instead of console.log()-JavaScript or System.out.println()-Java
 
-replace_word()
+replace_word()#Lack of indentation means exit previous method ##Call method created
