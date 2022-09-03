9d3aba49d4fa2e284a607ecc0733928709455fba
David Moina <davemolina777@gmail.com>
2022-09-03 16:02:29 -0500

    Create first mini project

---
+++ word_replacement_program.py
@@ -0,0 +1,8 @@
+def replace_word():
+    
+    str = "Hi guys, I am David, and hi hi hi hi"
+    word_to_replace = input("Enter the word you wish to replace")
+    word_replacer = input("Enter new word taking place")
+    print(str.replace(word_to_replace, word_replacer))
+
+replace_word()
